1430eb71a3af9dddf6b65fa297adcd61f16e5645
Steffen Nuessle <nuessle.steffen@gmail.com>
2022-06-12 14:15:34 +0200

    Added basic unit test setup
    The added test's main purpose is to show that the unit test build
    automation works as expected.

---
+++ test/unit/util/string-ostream.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022   Steffen Nuessle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <util/string-ostream.hpp>
+
+
+TEST(string_ostream, constructor)
+{
+    util::string_ostream ostream;
+
+    ASSERT_EQ(BUFSIZ, ostream.str().capacity());
+}
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
